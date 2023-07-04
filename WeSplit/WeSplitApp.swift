8da39ba85ec2d4c8a667f3f981f700fa910d4d7d
Induwara <kandapah@ualberta.ca>
2023-07-04 01:01:22 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Induwara on 2023-07-04.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
